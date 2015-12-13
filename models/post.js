9dc4f5da67d02231b7a439492a062df0446e4f15
HJhoselinavances <jhosi.htj@gmail.com>
2015-12-13 10:11:04 +0100

    examen final

---
+++ models/post.js
@@ -0,0 +1,9 @@
+//Escriba la estructura de la tabla dentro de las llaves
+module.exports= function(sequelize, DataTypes){
+	return sequelize.define('Post', {  apellidoPaterno: DataTypes.STRING,
+									   Post: DataTypes.STRING
+							
+									  
+									   });
+
+}
